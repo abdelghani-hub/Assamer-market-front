ac2db8ca5cbce8bd32d969d841202525ffdff824
abdelghani <aaittamghart9@gmail.com>
2025-03-05 12:06:05 +0000

    feat: add hero section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,31 @@
 import {Component, OnInit} from '@angular/core';
+import {NgIf} from '@angular/common';
+import {RouterLink} from '@angular/router';
+import {AuthService} from '../../core/services/auth.service';
+import User from '../../types/User';
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [
+    NgIf,
+    RouterLink
+  ],
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
 
+  private authService: AuthService;
+
+  constructor(authService: AuthService) {
+    this.authService = authService;
+  }
+
   ngOnInit(): void {
 
+  }
+
+  get user(): User | null {
+    return this.authService.user;
   }
 }
